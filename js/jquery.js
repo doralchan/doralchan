3ec548e6e032ea9811f682f66b553ff0be6c0251
Dora Chan <dora.l.chan@gmail.com>
2013-10-05 01:08:17 -0700

    Iteration

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-	$(".greeting").hide(function(){});
 	$(".content").mouseenter(function(){
-		$(".greeting").fadeIn(1400);
+		$(".greeting").fadeIn(2000);
+		$(".greeting").delay(8000).fadeOut(2000);
 	});
+	$(".more").click(function(){
+		$(".middlecontent p").slideToggle("slow");
+		$(this).fadeOut("fast");
+	});
+
 });
+
+
+// '.image-thumbnails' - project thumbnails
+// 'lightbox-img' - project featured image
+// 'lightbox-showing' - project page
+
+//var images = [
+//	{ src: 'css/darkblur.jpg', thumb: 'css/darkblur-small.jpg', index: 0 },
+//	{ src: 'css/darkblur.jpg', thumb: 'css/darkblur-small.jpg', index: 1 }];
