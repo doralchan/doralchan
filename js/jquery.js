480901cac4480257ef3d64dae9fc6441432f4b67
Dora Chan <dora.l.chan@gmail.com>
2013-10-20 10:18:35 -0700

    Contact

--- js/jquery.js
+++ js/jquery.js
@@ -40,8 +40,8 @@ $(document).ready(function(){
 	});
 
 	$("a[href='#navbar']").click(function() {
-	$("html, body").animate({ scrollTop: 0 });
-	return false;
+		$("html, body").animate({ scrollTop: 0 });
+		return false;
 	});
 
 	$(".moremail").click(function(){
@@ -58,34 +58,32 @@ $(document).ready(function(){
 	});
 
 		$(window).scroll( function(){
-		$(".bottomcontent img:nth-child(1)").each(function(){
-		var bottom_of_object = $(this).position().top + $(this).outerHeight();
-		var bottom_of_window = $(window).scrollTop() + $(window).height();
-		if( bottom_of_window = bottom_of_object ){
-			$(this).animate({opacity:1},1500);
-			$(".bottomcontent img:nth-child(2)").delay(1500).animate({opacity:0.3},1000);
-			$(this).delay(1500).animate({opacity:0.35},900);
-			$(".bottomcontent img:nth-child(3)").delay(3200).animate({opacity:1},850);
-			$(".bottomcontent img:nth-child(4)").delay(3100).animate({opacity:1},850);
-			$(".bottomcontent img:nth-child(2)").delay(800).animate({opacity:0.1},850);
-			$(window).off("scroll");
-			}
+			$(".bottomcontent img:nth-child(1)").each(function(){
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if( bottom_of_window = bottom_of_object ){
+				$(this).animate({opacity:1},1500);
+				$(".bottomcontent img:nth-child(2)").delay(1500).animate({opacity:0.3},1000);
+				$(this).delay(1500).animate({opacity:0.35},900);
+				$(".bottomcontent img:nth-child(3)").delay(3200).animate({opacity:1},850);
+				$(".bottomcontent img:nth-child(4)").delay(3100).animate({opacity:1},850);
+				$(".bottomcontent img:nth-child(2)").delay(800).animate({opacity:0.1},850);
+				$(window).off("scroll");
+				}
 			});
-			});
+		});
 
-    $(".image-thumbnails img").click(function(){
-    	$(".lightbox").addClass("lightbox-showing");
-    	$(".lightbox-img").attr("src", $(this).data('large'));
-    	$(".lightbox-exit img").click(function(){
-    		$(".lightbox").removeClass("lightbox-showing");
-    	});
-    });
+	$(".image-thumbnails img").click(function(){
+		$(".lightbox").addClass("lightbox-showing");
+		$(".lightbox-img").attr("src", $(this).data('large'));
+		$(".container").hide();
+		$(".lightbox-exit img").click(function(){
+			$(".lightbox").removeClass("lightbox-showing");
+			$(".container").show();
+		});
+	});
 
 });
-
-// '.image-thumbnails' - project thumbnails
-// 'lightbox-img' - project featured image
-// 'lightbox-showing' - project page
 
 //var images = [
 //	{ src: 'css/darkblur.jpg', thumb: 'css/darkblur-small.jpg', index: 0 },
