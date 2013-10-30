c8a08a1373b909f6cda4b9b069fb108f9de2cb8f
Dora Chan <dora.l.chan@gmail.com>
2013-10-29 17:06:32 -0700

    Coordinates!

--- js/test.js
+++ js/test.js
@@ -39,3 +39,13 @@ $(".canvas").mousemove(function(event){
 	var y = event.clientY - this.offsetTop;
 	$(".point").text("your x position is " + x + "and your y position is " + y);
 });
+
+$(".canvas").mousemove(function(event){
+	var x = event.clientX - this.offsetLeft;
+	var y = event.clientY - this.offsetTop;
+	if (130 <= x && x <=150 && 430 <= y && y <=450){
+		$(".canvas li:nth-child(2)").show();
+	}else{
+		$(".canvas li:nth-child(2)").hide();
+	};
+});
