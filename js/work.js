7c52a652a7ae39520281249e2dfc9611e5e4ce58
Dora Chan <dora.l.chan@gmail.com>
2013-11-03 16:59:32 -0800

    Cam

--- js/work.js
+++ js/work.js
@@ -1,30 +1,60 @@
 $(document).ready(function(){
 
-	$(".image-thumbnails img").click(function(){
-		$(".lightbox").addClass("lightbox-showing");
-		$(".lightbox-img").attr("src", $(this).data('project'));
-		$("body").css("overflow", "hidden");
-		$(".lightbox-exit img").click(function(){
-			$(".lightbox").removeClass("lightbox-showing");
-			$("body").css("overflow", "auto");
-			$(".container").show();
-		});
+	var projectsArray = {
+		1: ["jpg/project1.jpg", "jpg/project1-A.jpg", "jpg/project1-B.jpg"],
+		2: ["jpg/project2-A.jpg", "jpg/project2-B.jpg"],
+		3: ["jpg/project3-A.jpg", "jpg/project3-B.jpg"],
+		4: ["jpg/project4-A.jpg", "jpg/project4-B.jpg"],
+	};
 
-		var i = 0;
-		var projectArray = {
-			1: ["jpg/project1-A.jpg", "jpg/project1-B.jpg"],
-			2: ["jpg/project2-A.jpg", "jpg/project2-B.jpg"],
-			3: ["jpg/project3-A.jpg", "jpg/project3-B.jpg"],
-			4: ["jpg/project4-A.jpg", "jpg/project4-B.jpg"],
-		};
+	var currentIndex = 0;
+	var currentProject;
 
-		function imageChange(){
-			if(i % projectArray.length)
-			{i = 0;}
-			$(".lightbox-img").attr("src", projectArray[1][i]);
-			i++;
+	$(".image-thumbnails img").click(function () {
+		var projectName = $(this).data('project');
+		currentProject = projectsArray[projectName];
+		if (!currentProject) {
+			return;
 		}
-		$("#lightbox-navright img").click(imageChange);
-
+		showLightbox();
 	});
+
+	function showLightbox() {
+		$(".lightbox").addClass("lightbox-showing");
+		$("body").css("overflow", "hidden");
+		$(".lightbox-exit img").on('click', hideLightbox);
+
+		$("#lightbox-navright img").on('click', showNext);
+		$("#lightbox-navleft img").on('click', showPrev);
+
+		showImage(0);
+	}
+
+	function showPrev() {
+		showImage(currentIndex - 1);
+	}
+
+	function showNext() {
+		showImage(currentIndex + 1);
+	}
+
+	function showImage(index) {
+		currentIndex = clamp(index, 0, currentProject.length - 1);
+		$("#lightbox-navleft img").toggle(currentIndex > 0);
+		$("#lightbox-navright img").toggle(currentIndex < currentProject.length - 1);
+		$(".lightbox-img").attr("src", currentProject[currentIndex]);
+	}
+
+	function clamp(x, a, b) {
+		return x < a ? a : x > b ? b : x;
+	}
+
+	function hideLightbox() {
+		$(".lightbox").removeClass("lightbox-showing");
+		$("body").css("overflow", "auto");
+		$(".container").show();
+		$("#lightbox-navright img").off('click', showNext);
+		$("#lightbox-navleft img").off('click', showPrev);
+		$(".lightbox-exit img").off('click', hideLightbox);
+	}
 });
