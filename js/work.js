ea81fa17915e9e822cb79e96385d47a909b85d04
Dora Chan <dora.l.chan@gmail.com>
2013-11-09 22:37:53 -0800

    Updates

--- js/work.js
+++ js/work.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
 		4: ["jpg/project4-A.jpg", "jpg/project4-B.jpg"],
 		5: ["jpg/project5-A.jpg", "jpg/project5-B.jpg", "jpg/project5-C.jpg", "jpg/project5-D.jpg", "jpg/project5-E.jpg", "jpg/project5-F.jpg"],
 		6: ["jpg/project6-A.jpg", "jpg/project6-B.jpg"],
-		7: ["jpg/project7-A.jpg", "jpg/project7-B.jpg", "jpg/project7-C.jpg", "jpg/project7-D.jpg", "jpg/project7-E.jpg"],
+		7: ["jpg/project7-A.jpg", "jpg/project7-B.jpg", "jpg/project7-C.jpg", "jpg/project7-D.jpg", "jpg/project7-E.jpg", "jpg/project7-F.jpg", "jpg/project7-G.jpg", "jpg/project7-H.jpg", "jpg/project7-I.jpg", "jpg/project7-J.jpg", "jpg/project7-K.jpg"],
 		8: ["jpg/project8-A.jpg", "jpg/project8-B.jpg", "jpg/project8-C.jpg", "jpg/project8-D.jpg", "jpg/project8-E.jpg", "jpg/project8-F.jpg", "jpg/project8-G.jpg", "jpg/project8-H.jpg"],
 		9: ["jpg/project9-A.jpg", "jpg/project9-B.jpg"],
-		10: ["jpg/project10-A.jpg", "jpg/project10-B.jpg", "jpg/project10-C.jpg", "jpg/project10-D.jpg", "jpg/project10-E.jpg", "jpg/project10-F.jpg"],
+		10: ["jpg/project10-A.jpg", "jpg/project10-B.jpg", "jpg/project10-C.jpg", "jpg/project10-D.jpg", "jpg/project10-E.jpg", "jpg/project10-F.jpg", "jpg/project10-G.jpg"],
 		11: ["jpg/project11-A.jpg", "jpg/project11-B.jpg"],
 		12: ["jpg/project12-A.jpg", "jpg/project12-B.jpg", "jpg/project12-C.jpg", "jpg/project12-D.jpg", "jpg/project12-E.jpg", "jpg/project12-F.jpg"],
 		13: ["jpg/project13-A.jpg", "jpg/project13-B.jpg"],
@@ -18,8 +18,8 @@ $(document).ready(function(){
 		15: ["jpg/project15-A.jpg"],
 		16: ["jpg/project16-A.jpg", "jpg/project16-B.jpg"],
 		17: ["jpg/project17-A.jpg", "jpg/project17-B.jpg"],
-		18: ["jpg/project18-A.jpg", "jpg/project18-B.jpg"],
-		19: ["jpg/project19-A.jpg", "jpg/project19-B.jpg"],
+		18: ["jpg/project18-A.jpg"],
+		19: ["jpg/project19-A.jpg"],
 		20: ["jpg/project20-A.jpg", "jpg/project20-B.jpg"],
 	};
 
