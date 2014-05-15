7fe0f3ef59494056dc5016450095c050954a1bf5
Dora Chan <dora.l.chan@gmail.com>
2014-05-15 03:18:54 -0700

    Save

---
+++ js/effects.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+$(window).resize(function() {
+    location.reload(true);
+});
+
+  $("#appslide").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 300,
+      singleItem:true
+
+  });
+
+});
