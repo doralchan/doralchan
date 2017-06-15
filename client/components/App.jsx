405cecd8b16c2c9162d8178866982964a31a77ab
Dora Chan <dora.l.chan@gmail.com>
2017-06-14 21:49:31 -0700

    set up webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from './assets/illo-item-1.svg';
+
+class App extends React.Component {
+  render() {
+    return (
+     <div>test</div>
+   );
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|eot|woff|woff|png|svg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
