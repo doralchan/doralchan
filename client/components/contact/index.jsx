826d866b5718c850d3e372fe78eaabbec6a14be1
Dora Chan <dora.l.chan@gmail.com>
2017-10-16 22:38:17 -0700

    added contact

--- client/components/contact/index.jsx
+++ client/components/contact/index.jsx
@@ -7,12 +7,35 @@ import Number from '../../shared/number/index.jsx';
 import './styles.scss';
 
 class Contact extends React.Component {
+  renderLinkedIn() {
+    return (
+      <span>
+        <a href="https://www.linkedin.com/in/doralchan" target='blank' className='contact-area-link'>LinkedIn</a>
+      </span>
+    );
+  }
+
+  renderTwitter() {
+    return (
+      <span>
+        <a href="https://twitter.com/doralchan" target='blank' className='contact-area-link'>@doralchan</a>
+      </span>
+    );
+  }
+
   render() {
     return (
       <Section idName='contact'>
-        <ContentBlock title='contact me'>
-          TBD
-        </ContentBlock>
+        <div className='contact-area'>
+          <ContentBlock title='contact me'>
+            If you'd like to chat or ask me about anything above, feel free
+            to contact me through { this.renderLinkedIn() } or DM me at my
+            Twitter handle { this.renderTwitter() }.
+          </ContentBlock>
+        </div>
+        <div className='contact-number'>
+          <Number idNumber={ 5 } />
+        </div>
       </Section>
     );
   }
