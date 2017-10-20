8b8da5329df88bf5c1787219ddc46145784c39dc
Dora Chan <dora.l.chan@gmail.com>
2017-10-20 09:22:57 -0700

    adding new images

--- client/components/projects/gusto.jsx
+++ client/components/projects/gusto.jsx
@@ -14,58 +14,34 @@ import GuideResearch from '../../assets/images/guide_research_interviews.png';
 import GuideActions from '../../assets/images/guide_usability_actions.png';
 import GuideWorkflows from '../../assets/images/guide_usability_workflows.png';
 
+import ChatOffPro from '../../assets/images/chat_offline_proactive.png';
+import ChatOffRea from '../../assets/images/chat_offline_reactive.png';
+import ChatOnPro from '../../assets/images/chat_online_proactive.png';
+import ChatOnReaA from '../../assets/images/chat_online_reactive_a.png';
+import ChatOnReaB from '../../assets/images/chat_online_reactive_b.png';
+
 import './styles.scss';
 
 class Gusto extends React.Component {
-  renderOnboarding() {
-    return (
-      <Carousel>
-        <Slide title='onboarding'>
-          <Slide.Block subtitle='project context'>
-
-          </Slide.Block>
-          <Slide.Block subtitle='project goal'>
-            The motivation was to fill these knowledge gaps not only for
-            designers and developers, but for the rest of the company as well.
-            I wanted to create a long term solution that could evolve as the
-            company evolves.
-          </Slide.Block>
-          <Slide.Block subtitle='project direction'>
-            After researching the biggest internal pain points, I recognized
-          </Slide.Block>
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-      </Carousel>
-    );
-  }
-
   renderGuide() {
     return (
       <Carousel>
-        <Slide title='the guide'>
-          <Slide.Block subtitle='project context'>
+        <Slide title='The Guide'>
+          <Slide.Block subtitle='Project Context'>
             With the rapid growth and rebranding of the company, large
             inconsistencies were developing between teams. There was uncertainty
             about what UI components, UX paradigms, and assets were available to
             product and marketing. Undocumented, tribal knowledge became the norm.
           </Slide.Block>
-          <Slide.Block subtitle='project goal'>
+          <Slide.Block subtitle='Project Goal'>
             The motivation was to fill these knowledge gaps for a majority of
             the teams including designers, developers, product managers, marketing,
             and sales. I wanted to create a long term solution that would
             constantly reflect the evolving state of the company.
           </Slide.Block>
-          <Slide.Block subtitle='project direction'>
-            To further investigate painpoints beyond those of my own team, I sent
-            out surveys to
+          <Slide.Block subtitle='Project Direction'>
+            To further investigate issues beyond those of my own team, I sent
+            out surveys to understand the needs of the company.
           </Slide.Block>
         </Slide>
         <Slide><Image imageLink={ GuideHome } /></Slide>
@@ -79,35 +55,44 @@ class Gusto extends React.Component {
     );
   }
 
+  renderOnboarding() {
+    return (
+      <Carousel>
+        <Slide title='Onboarding'>
+          <Slide.Block subtitle='Project Context'>
+            TBD
+          </Slide.Block>
+          <Slide.Block subtitle='Project Goal'>
+            TBD
+          </Slide.Block>
+          <Slide.Block subtitle='Project Direction'>
+            TBD
+          </Slide.Block>
+        </Slide>
+        <Slide>TBD</Slide>
+      </Carousel>
+    );
+  }
+
   renderChat() {
     return (
       <Carousel>
-        <Slide title='chat'>
-          <Slide.Block subtitle='project context'>
-            With the rapid growth of the company, large inconsistencies were
-            developing between mission teams. There was uncertainty in what
-            UI was available, what paradigms were available, and where assets
-            were stored.
+        <Slide title='Chat'>
+          <Slide.Block subtitle='Project Context'>
+            TBD
           </Slide.Block>
-          <Slide.Block subtitle='project goal'>
-            The motivation was to fill these knowledge gaps not only for
-            designers and developers, but for the rest of the company as well.
-            I wanted to create a long term solution that could evolve as the
-            company evolves.
+          <Slide.Block subtitle='Project Goal'>
+            TBD
           </Slide.Block>
-          <Slide.Block subtitle='project direction'>
-            After researching the biggest internal pain points, I recognized
+          <Slide.Block subtitle='Project Direction'>
+            TBD
           </Slide.Block>
         </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
+        <Slide><Image imageLink={ ChatOnReaA } /></Slide>
+        <Slide><Image imageLink={ ChatOnReaB } /></Slide>
+        <Slide><Image imageLink={ ChatOnPro } /></Slide>
+        <Slide><Image imageLink={ ChatOffPro } /></Slide>
+        <Slide><Image imageLink={ ChatOffRea } /></Slide>
       </Carousel>
     );
   }
@@ -115,32 +100,22 @@ class Gusto extends React.Component {
   renderGarnishments() {
     return (
       <Carousel>
-        <Slide title='garnishments'>
-          <Slide.Block subtitle='project context'>
-            With the rapid growth of the company, large inconsistencies were
-            developing between mission teams. There was uncertainty in what
-            UI was available, what paradigms were available, and where assets
-            were stored.
+        <Slide title='Garnishments'>
+          <Slide.Block subtitle='Project Context'>
+            TBD
           </Slide.Block>
-          <Slide.Block subtitle='project goal'>
-            The motivation was to fill these knowledge gaps not only for
-            designers and developers, but for the rest of the company as well.
-            I wanted to create a long term solution that could evolve as the
-            company evolves.
+          <Slide.Block subtitle='Project Goal'>
+            TBD
           </Slide.Block>
-          <Slide.Block subtitle='project direction'>
-            After researching the biggest internal pain points, I recognized
+          <Slide.Block subtitle='Project Direction'>
+            TBD
           </Slide.Block>
         </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
-        <Slide>
-          This is an image slide
-        </Slide>
+        <Slide><Image imageLink={ ChatOnReaA } /></Slide>
+        <Slide><Image imageLink={ ChatOnReaB } /></Slide>
+        <Slide><Image imageLink={ ChatOnPro } /></Slide>
+        <Slide><Image imageLink={ ChatOffPro } /></Slide>
+        <Slide><Image imageLink={ ChatOffRea } /></Slide>
       </Carousel>
     );
   }
@@ -150,11 +125,11 @@ class Gusto extends React.Component {
       <Fade top>
         <div className='panel-group'>
           <Panel panelName='gusto' />
-          <Panel panelProject='onboarding'>
-            { this.renderOnboarding() }
-          </Panel>
           <Panel panelProject='the guide'>
             { this.renderGuide() }
+          </Panel>
+          <Panel panelProject='onboarding'>
+            { this.renderOnboarding() }
           </Panel>
           <Panel panelProject='chat'>
             { this.renderChat() }
